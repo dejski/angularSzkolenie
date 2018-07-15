ecfa78d5c005f13802ee0c086e23ad81ae5788b4
dejski <dejski@wp.pl>
2018-07-15 14:49:27 +0200

    zmiana w gałęzi

--- src/app/cars/cars-list/cars-list.component.ts
+++ src/app/cars/cars-list/cars-list.component.ts
@@ -15,7 +15,7 @@ export class CarsListComponent implements OnInit {
       deliveryDate: '21-04-2017',
       deadline: '05-05-2016',
       client: {
-        firstName: 'Jan',
+        firstName: 'Janek',
         surname: 'Kowalski'
       },
       cost: 300,
